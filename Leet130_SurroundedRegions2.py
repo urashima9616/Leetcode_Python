ddae5c32f6649d6cb0ff9e79928f4bfa0fc52ca0
urashima9616 <urashima9616@gmail.com>
2017-03-06 16:45:41 -0800

    update 130

--- Leet130_SurroundedRegions2.py
+++ Leet130_SurroundedRegions2.py
@@ -53,3 +53,6 @@ board = [['X','X','X','X'], ['X', 'O', 'O','X'], ['X','X','O','X'],['X','O','X',
 #board = [["O","X", "X","O","X"], ["X","O","O","X","O"], ["X","O","X","O","X"], ["O","X","O","O","O"],["X","X","O","X","O"]]
 Solve.solve(board)
 print "done"
+
+
+
