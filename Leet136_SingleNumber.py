ed0da0a519a785c45ed90a1f226727aeeaf5e97f
urashima9616 <urashima9616@gmail.com>
2017-03-11 18:58:50 -0800

    comments on in-place solution of Leet136

--- Leet136_SingleNumber.py
+++ Leet136_SingleNumber.py
@@ -3,6 +3,7 @@ Given an array of integers, every element appears twice except for one. Find tha
 
 Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+#In-place solution takes XOR for all entries in the list. The remaining result is the number to find
 """
 class Solution(object):
     def singleNumber(self, nums):
